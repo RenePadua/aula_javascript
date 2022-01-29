a0058c9b11e1bb60ec4d3645bc5f61da1b90025a
unknown <rene.padua@gmail.com>
2022-01-29 00:22:02 -0300

    Primeira alteração

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+/*
+var d = new Date();
+alert(d);
+alert(d.getDay());
+alert(d.getDate());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = [nome: "maça", cor: "vermelha"];
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//var nome = "Rene Padua";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade +  " anos");
+//alert(idade+idade2);
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
